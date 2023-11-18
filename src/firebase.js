d442486d6fa9acda955859e6f965986a25b3d141
Usmonov Hojiakbar <usmonovmaxsudovich@gmail.com>
2023-11-19 03:10:27 +0900

    Firebase and AuthContext

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,9 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+/* import firebase from 'firebase/app';
+import 'firebase/auth'; */
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 export const auth = firebase
 	.initializeApp({
@@ -11,3 +15,5 @@ export const auth = firebase
 		appId: '1:1084522566155:web:2c78d7947b97a1d9956d0e',
 	})
 	.auth();
+
+export default firebase;
